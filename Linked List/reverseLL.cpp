18948c00d426729816184ba5784c63955b4437e2
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-05-06 17:04:43 +0530

    Update reverseLL.cpp

--- Linked List/reverseLL.cpp
+++ Linked List/reverseLL.cpp
@@ -1,3 +1,5 @@
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< BY ITERATIVE APPROACH >>>>>>>>>>>>>>>>>>>>>>>>>>
+
 class Solution
 {
     public:
@@ -19,6 +21,45 @@ class Solution
       curr = forward;
      }      
         return prev;
+    }
+    
+};
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<< BY RECURSION APPROACH >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+class Solution
+{
+    public:
+    
+    
+    void reverse( Node *&head, Node *&current, Node*&prev){
+        
+        if(current == NULL){
+          //  head = prev;
+            return ;
+        }
+        Node *forward = current->next;
+        current->next = prev;
+        reverse(head, forward, current);
+        prev=current;
+    
+    }
+    
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *&head)
+    {
+        // code here
+        // return head of reversed list
+        
+        Node *current = head;
+        Node *prev = NULL;
+        
+        reverse(head, current , prev);
+        
+        return prev;
+        
+        
+        
     }
     
 };
