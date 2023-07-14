777e328b8a05e1c526a21f18e0f4c1b5519ef06c
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-07-14 12:13:37 +0530

    Create firstNodeofLoop.cpp

---
+++ Linked List/firstNodeofLoop.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+    public:
+     //Function to find first node if the linked list has a loop.
+    int findFirstNode(Node* head)
+    {
+        // your code here
+        Node *slow = head;
+        Node *fast = head;
+        
+        while(slow != NULL && fast != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){
+                break;
+            }
+        }
+        
+        if(slow != fast){
+            return -1;
+        }
+        
+     slow = head;
+     while(slow != fast){
+         slow = slow->next;
+         fast = fast->next;
+     }
+        
+     return slow->data;
+        
+    }
+};
