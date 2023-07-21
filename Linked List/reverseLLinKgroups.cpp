10dce931197474b3be740842858722fc778297fd
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-07-21 11:41:01 +0530

    Update reverseLLinKgroups.cpp

--- Linked List/reverseLLinKgroups.cpp
+++ Linked List/reverseLLinKgroups.cpp
@@ -7,8 +7,11 @@ class Solution
     struct node *reverse (struct node *head, int k)
     { 
         // Complete this method
-        
-        
+        if(head == NULL{
+        return NULL;
+        }        
+
+        // reverse first k node
         node *current = head;
         node *prev = NULL;
         node *forward = NULL;
@@ -22,7 +25,9 @@ class Solution
             current = forward;
             count++;
         }
-        
+
+
+        // reverse remaining k nodes
         if(forward != NULL)
     {
         head->next = reverse(forward,k);
