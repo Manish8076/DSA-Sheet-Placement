251f3f35b28c4df70d4609ffb6b1d95869be19e6
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-07-29 15:03:50 +0530

    Create nextSmallerElement.cpp

---
+++ StackAndQueue/nextSmallerElement.cpp
@@ -0,0 +1,28 @@
+
+
+  void nextSmallerElement(vector<int>&nums1,vector<int>& nums2) {
+        unordered_map<int,int>m;
+        stack<int>st;
+        int n = nums2.size();
+
+        for(int i = 0; i<n; i++){
+            int element = nums2[i];
+           
+           while(!st.empty() && st.top()>=element){
+               st.pop();
+           }
+           int res = (st.empty())? -1 : st.top();
+           m.insert({element , res});
+           st.push(element);
+        }
+
+        vector<int>ans;
+        for(auto x : nums1){
+           ans.push_back(m[x]);
+        }
+      
+       for(int i =0; i<ans.size(); i++){
+           cout<<ans[i]<<" ";
+       }
+      
+    }
