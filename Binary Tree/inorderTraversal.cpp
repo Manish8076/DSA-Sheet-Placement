f94e88ea81f2d0eb76a34b708a994b3f3603d8b0
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-08-08 13:28:16 +0530

    Create inorderTraversal.cpp

---
+++ Binary Tree/inorderTraversal.cpp
@@ -0,0 +1,20 @@
+
+
+class Solution {
+public:
+
+    void inorder(TreeNode *&root, vector<int>&ans){
+       if(root!=NULL){
+           inorder(root->left,ans);
+           ans.push_back(root->val);
+           inorder(root->right,ans);
+       }
+
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        inorder(root,ans);       
+       return ans; 
+    }
+};
