85e413464877c16181eaa995338d9b130a49636b
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-08-10 17:26:01 +0530

    Create topViewofBT.cpp

---
+++ Binary Tree/topViewofBT.cpp
@@ -0,0 +1,56 @@
+
+
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        //Your code here
+        // Your Code Here
+    vector<int>ans;
+    
+    
+    if(root== NULL){
+        return ans;
+    }
+    
+    
+    map<int,int>topnode;
+    queue<pair<Node *, int>>q;
+    
+    
+    q.push(make_pair(root,0));
+    
+    while(!q.empty()){
+        
+    pair<Node*, int> temp = q.front();
+    q.pop();
+    
+    Node *frontnode = temp.first;
+    int hd = temp.second;
+    
+     if(topnode.find(hd) == topnode.end()){
+         topnode[hd] = frontnode->data;
+     }
+    
+    
+    if(frontnode->left){
+        q.push(make_pair(frontnode->left, hd-1));
+    }
+    
+    if(frontnode->right){
+        q.push(make_pair(frontnode->right, hd+1));
+    }
+    
+    }
+    
+    for(auto i : topnode){
+        ans.push_back(i.second);
+    }
+    return ans;
+    }
+
+
+};
