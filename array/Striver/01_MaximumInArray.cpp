77b49af3e5a63fe834fcf18e42f58aa3c94d5002
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-12-23 14:31:35 +0530

    Create 01_MaximumInArray.cpp

---
+++ array/Striver/01_MaximumInArray.cpp
@@ -0,0 +1,14 @@
+
+
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    // Write your code here.
+     int ans=0;
+
+    for(int i=0; i<arr.size(); i++){
+        if(ans<arr[i]){
+            ans = arr[i];
+        }
+    }
+    return ans;
+}
