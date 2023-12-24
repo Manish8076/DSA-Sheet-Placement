2890ab53c742f4c1ee66bbb8db7ed45b8d3f5a67
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2023-12-24 15:13:23 +0530

    Update 01_MaximumInArray.cpp

--- array/Striver/01_MaximumInArray.cpp
+++ array/Striver/01_MaximumInArray.cpp
@@ -1,5 +1,17 @@
+/* <<<<<<<=================== Approach 1 =======================>>>>>>>>>>>>*/
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    // Write your code here.
+     sort(arr.begin(), arr.end());
+
+    return arr[n-1];
+}
 
 
+
+
+
+/* <<<<<==================== Approach 2 =======================>>>>>>>>> */
 #include <bits/stdc++.h> 
 int largestElement(vector<int> &arr, int n) {
     // Write your code here.
