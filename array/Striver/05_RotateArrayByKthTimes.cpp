95cd1fc6b05358fe4ed3165dc08bc6e4b9918bd6
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-02-03 12:18:35 +0530

    Create 05_RotateArrayByKthTimes.cpp

---
+++ array/Striver/05_RotateArrayByKthTimes.cpp
@@ -0,0 +1,32 @@
+
+
+    void reverse(vector<int>&nums, int s, int e){
+
+        while(s<e){
+            swap(nums[s],nums[e]);
+            s++;
+            e--;
+        }
+
+    }
+
+    void rotate(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        
+        if(n==0 || n ==1){
+            return;
+        }
+
+        k = k%n;
+
+        reverse(nums, 0, n-k-1);
+        reverse(nums,n-k, n-1);
+        reverse(nums, 0, n-1);
+        
+        
+    }
+
+// Time Complexity :- O(N)
+// Space Complexity :- O(1)
+
