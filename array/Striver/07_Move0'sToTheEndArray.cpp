6058b6a4ea2b27d7d5fd653aacaa527b78223b86
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-02-04 12:54:36 +0530

    Create 07_Move0'sToTheEndArray.cpp

---
+++ array/Striver/07_Move0'sToTheEndArray.cpp
@@ -0,0 +1,43 @@
+
+
+    void moveZeroes(vector<int>& nums) {
+    //         int j = -1;
+    //         int n = a.size();
+    // //place the pointer j:
+    // for (int i = 0; i < n; i++) {
+    //     if (a[i] == 0) {
+    //         j = i;
+    //         break;
+    //     }
+    // }
+
+    // //no non-zero elements:
+    // if (j == -1) return;
+
+    // //Move the pointers i and j
+    // //and swap accordingly:
+    // for (int i = j + 1; i < n; i++) {
+    //     if (a[i] != 0) {
+    //         swap(a[i], a[j]);
+    //         j++;
+    //     }
+    // }
+
+    int n = nums.size();
+    int j =0;
+
+    for(int i =0; i<n; i++){
+        if(nums[i]!= 0){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    for(;j<n; j++){
+        nums[j] = 0;
+    }
+    }
+
+// For both the apporach the complexity is same 
+// Time Complexity :- O(N)
+// Scpace Complexity :- O(N) 
+
