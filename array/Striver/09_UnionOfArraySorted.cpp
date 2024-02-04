f8c50cc0194a8e96a27099df8107a26e0cada5a3
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-02-04 18:41:42 +0530

    Create 09_UnionOfArraySorted.cpp

---
+++ array/Striver/09_UnionOfArraySorted.cpp
@@ -0,0 +1,43 @@
+
+vector < int > sortedArray(vector < int > arr1, vector < int > arr2) {
+    // Write your code here
+
+      int i = 0, j = 0; // pointers
+      int n = arr1.size();
+      int m = arr2.size();
+
+
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+
+}
+
+
+
+// Time Complexity :- O(N+M)
+// Space Complexity :- O(N+M)
