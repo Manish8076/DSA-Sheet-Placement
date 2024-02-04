a229c51c3532a1962e0e721a13e78177b6305089
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-02-04 18:43:00 +0530

    Create 10_MissingArray.cpp

---
+++ array/Striver/10_MissingArray.cpp
@@ -0,0 +1,16 @@
+
+
+    int missingNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i =0; i<=nums.size(); i++){
+            ans = ans+i;
+        }
+
+        for(int i=0;i<nums.size(); i++){
+            ans = ans-nums[i];
+        }
+        return ans;
+    }
+
+// Time Complexity :- O(N)
+// Space Complexity :- O(1)
