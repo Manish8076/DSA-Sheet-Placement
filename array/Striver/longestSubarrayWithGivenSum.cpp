c8c0794b4adb5895350e997080cc73b932db15b3
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-02-08 14:38:50 +0530

    Create longestSubarrayWithGivenSum.cpp

---
+++ array/Striver/longestSubarrayWithGivenSum.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h> 
+int getLongestSubarray(vector<int>& a, int k){
+    // Write your code here
+            int n = a.size(); // size of the array.
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+// Time Coplexity :- O(N)
+// Space Complexity :- O(1)
+
+
+
