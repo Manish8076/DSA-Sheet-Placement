148c1481a882475f2116d5000e1b5d9de633c6bd
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-02-21 12:07:49 +0530

    Update 05_RotateArrayByKthTimes.cpp

--- array/Striver/05_RotateArrayByKthTimes.cpp
+++ array/Striver/05_RotateArrayByKthTimes.cpp
@@ -18,13 +18,32 @@
             return;
         }
 
-        k = k%n;
+                int n = nums.size();
+        
+        if(n==0 || n ==1){
+            return;
+        }
 
+        // Approach 1 :- Ek new vector ka use liya hai 
+        
+        // vector<int>temp(n);
+
+        // for(int i=0; i<n; i++){
+        //     temp[(i+k)%n] = nums[i];
+        // }
+        // nums = temp;
+        
+// Time Complexity :- O(N)
+// Space Complexity :- O(N)
+        
+        
+        // Approach 2 :- Reverse approach 
+        
+        k = k%n;
         reverse(nums, 0, n-k-1);
         reverse(nums,n-k, n-1);
         reverse(nums, 0, n-1);
-        
-        
+
     }
 
 // Time Complexity :- O(N)
