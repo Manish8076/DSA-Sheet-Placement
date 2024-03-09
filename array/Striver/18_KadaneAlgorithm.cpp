06406bffcbf6391a67a3cdb3d89f10550c7e024f
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-03-09 11:58:47 +0530

    Update 18_KadaneAlgorithm.cpp

--- array/Striver/18_KadaneAlgorithm.cpp
+++ array/Striver/18_KadaneAlgorithm.cpp
@@ -1,11 +1,14 @@
 
     int maxSubArray(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int temp = 0;
 
-        for(int i =0; i<nums.size(); i++){
-            temp = max(nums[i], nums[i]+temp);
-            maxi = max(temp,maxi);
+        int sum = 0;
+        int maxi = nums[0];
+        for(int i=0; i<nums.size();i++){
+            sum += nums[i];
+            maxi = max(sum, maxi);
+            if(sum < 0){
+                sum =0;
+            }
         }
         return maxi;
     }
