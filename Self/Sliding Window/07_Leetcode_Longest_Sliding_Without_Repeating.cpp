12025b754fde82fe5b6dff7f89dc8555d63d8588
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-08-16 23:53:37 +0530

    Create 07_Leetcode_Longest_Sliding_Without_Repeating.cpp

---
+++ Self/Sliding Window/07_Leetcode_Longest_Sliding_Without_Repeating.cpp
@@ -0,0 +1,33 @@
+    int lengthOfLongestSubstring(string s) {
+        int start =0;
+        int end = 0;
+        int n = s.size();
+        map<char,int>mp;
+        int ans = 0;
+
+        while(end<n){
+            mp[s[end]]++;
+            
+            if(mp.size()>(end-start+1)){
+                end++;
+            }
+            else if(mp.size() == (end-start+1)){
+                ans = max(ans,(end-start+1));
+                end++;
+            }
+            else if (mp.size()< (end-start+1) ){
+                while(mp.size()<(end-start+1)){
+                    mp[s[start]]--;
+                    if(mp[s[start]] == 0){
+                        mp.erase(s[start]);
+                    }
+                    start++;
+                }
+                end++;
+            }
+
+        }
+        return ans;
+    }
+// Time Complexity :- O(N)
+// Space Complexity :- O(N)
