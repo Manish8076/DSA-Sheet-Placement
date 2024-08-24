a71bc4a42af9f9a74f8384c812fba6e76fb8de35
Manish8076 <97782464+Manish8076@users.noreply.github.com>
2024-08-24 12:43:24 +0530

    Update 23_LeadersInArray.cpp

--- array/Striver/23_LeadersInArray.cpp
+++ array/Striver/23_LeadersInArray.cpp
@@ -1,19 +1,19 @@
 
-vector<int> superiorElements(vector<int>&a) {
-    // Write your code here.
-    int maxi = INT_MIN;
-    vector<int>ans;
-
-    for(int i =a.size()-1; i>=0; i--){
-        if(a[i] > maxi){
-            maxi = a[i];
-            ans.push_back(a[i]);
+    vector<int> leaders(int n, int arr[]) {
+        // Code here
+        vector<int>ans;
+        ans.push_back(arr[n-1]);
+        for(int i =n-2; i>=0; i--){
+            if(arr[i] >= ans.back()){
+                ans.push_back(arr[i]);
+            }
         }
+        
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+
     }
-
-    return ans;
-
-}
 
 // Time Complexity :- O(N)
 // Space Complexity :- O(1)
